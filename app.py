273edc48ee7e5371b9c1dc5b20c8852fd8b8dcf1
5-ean <seandowden@hotmail.co.uk>
2021-11-16 19:04:36 +0000

    Add navigation bar

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -19,9 +20,12 @@ mongo = PyMongo(app)
 
 
 @app.route("/")
-def index():
+def recipes():
     recipes = mongo.db.recipes.find()
-    return render_template("index.html", recipes=recipes)
+    return render_template("recipes.html", recipes=recipes)
+
+
+
 
 
 if __name__ == "__main__":
