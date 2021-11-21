d90ebdbe33bf7cc16e5933d210775dc5cae5e96c
5-ean <seandowden@hotmail.co.uk>
2021-11-21 00:06:32 +0000

    Able to delete recipes from the DB

--- app.py
+++ app.py
@@ -145,6 +145,13 @@ def edit_recipe(recipe_id):
     return render_template("edit_recipe.html", recipe=recipe, cuisines=cuisines)
 
 
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
+    flash("Task Successfully Deleted")
+    return redirect(url_for("recipes"))
+
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
