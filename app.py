621d8e3da21aaa9f5e6f17c8beac923c666b5764
5-ean <seandowden@hotmail.co.uk>
2021-11-22 10:19:44 +0000

    debug=False

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 from flask import (
-    Flask, flash, render_template, 
-    redirect, request, session, url_for)
+    Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -22,6 +21,9 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/recipes")
 def recipes():
+    """
+    Function to get all recipes and display to template
+    """
     recipes = list(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=recipes)
 
@@ -43,7 +45,7 @@ def register():
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
-
+        # Inserts username and password into users collect of the database
         register = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
@@ -67,7 +69,7 @@ def login():
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
+             existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
                     flash("Welcome, {}".format(
                         request.form.get("username")))
@@ -99,16 +101,21 @@ def profile(username):
     # fetches session user's username from db.
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-
+    # if session user = username open profile
     if session["user"]:
         my_recipes = list(mongo.db.recipes.find())
-        return render_template("profile.html", username=username, my_recipes=my_recipes)
+        return render_template(
+            "profile.html", username=username, my_recipes=my_recipes)
 
     return redirect(url_for("login"))
 
 
 @app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
+    """
+    Function takes list of key value pairs from user input,
+    and injects the values into the database
+    """
     if request.method == "POST":
         recipe = {
             "cuisine_type": request.form.get("cuisine_type"),
@@ -149,19 +156,21 @@ def edit_recipe(recipe_id):
 
     recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     cuisines = mongo.db.cuisine.find().sort("cuisine_type", 1)
-    return render_template("edit_recipe.html", recipe=recipe, cuisines=cuisines)
+    return render_template(
+        "edit_recipe.html", recipe=recipe, cuisines=cuisines)
 
 
 @app.route("/delete_recipe/<recipe_id>")
 def delete_recipe(recipe_id):
+    """
+    Function removes recipe from recipes collection viva button. 
+    """
     mongo.db.recipes.remove({"_id": ObjectId(recipe_id)})
     flash("Task Successfully Deleted")
     return redirect(url_for("recipes"))
 
 
-
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
-
+            debug=False)
